a7b986bb8f38886e4d113830203d508624f1fbc2
justine Brun <jbrun@ippon.fr>
2022-04-28 17:09:14 +0200

    add 03

--- fr/03/main.tf
+++ fr/03/main.tf
@@ -4,16 +4,16 @@ terraform {
     }
   }
 }
-
 provider "google" {
   project     = "subtle-builder-348511"
   region      = "us-central1"
-  zone        = "us-central1-c"
+  zone = "us-central1-c"
 }
 
 resource "google_compute_instance" "default" {
   name         = "terraform-instance"
   machine_type = "e2-micro"
+  count= 3
 
   labels = {
     creator = "ippon"
@@ -27,17 +27,12 @@ resource "google_compute_instance" "default" {
     }
   }
 
+
   network_interface {
-    network = google_compute_network.vpc_network.self_link
+    network = "default"
 
     access_config {
       // Ephemeral public IP
     }
   }
-}
-
-resource "google_compute_network" "vpc_network" {
-  project                 = "subtle-builder-348511"
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
 }
