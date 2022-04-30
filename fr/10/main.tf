b6da2370dde9469cbcc62a91fe280a5eb3d6e864
Sylvain Iribarne <siribarne.ippon.fr>
2022-04-30 18:02:24 +0200

    feat: add outputs

--- fr/10/main.tf
+++ fr/10/main.tf
@@ -109,3 +109,21 @@ resource "google_compute_firewall" "iap_to_ssh" {
     ports    = [22]
   }
 }
+
+#              _               _       
+#             | |             | |      
+#   ___  _   _| |_ _ __  _   _| |_ ___ 
+#  / _ \| | | | __| '_ \| | | | __/ __|
+# | (_) | |_| | |_| |_) | |_| | |_\__ \
+#  \___/ \__,_|\__| .__/ \__,_|\__|___/
+#                 | |                  
+#                 |_|                  
+output "instance_public_ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
+}
+output "instance_private_ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+}
+output "instance_id" {
+  value = google_compute_instance.vm_instance.id
+}
